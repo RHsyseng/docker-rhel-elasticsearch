bfce14c45033c6931f1121cd5eea7aff80ddc08a
Ruben Romero Montes <rromerom@redhat.com>
2018-02-08 18:42:11 +0100

    Run ES in Kubernetes

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+set -o nounset
+
+# list of plugins to be installed
+if [ -z "${ES_CLOUD_K8S_URL:-}" ] ; then
+    ES_CLOUD_K8S_URL=io.fabric8:elasticsearch-cloud-kubernetes:${ES_CLOUD_K8S_VER}
+fi
+es_plugins=($ES_CLOUD_K8S_URL)
+
+echo "ES plugins: ${es_plugins[@]}"
+for es_plugin in ${es_plugins[@]}
+do
+  ${ES_HOME}/bin/elasticsearch-plugin install $es_plugin
+done
+
+mkdir /elasticsearch
+chmod -R og+w ${ES_PATH_CONF} ${ES_HOME} ${HOME} /elasticsearch
