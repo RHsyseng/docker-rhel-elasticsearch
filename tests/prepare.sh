ee4f0872f05c5b7ec16ed2ae97cbe75b95465bda
Ruben Romero Montes <rromerom@redhat.com>
2018-04-12 15:32:30 +0200

    Added probes and travis integration

---
+++ tests/prepare.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export PROJECT_NAME=docker-rhel-elasticsearch
+
+function prepare() {
+  sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
+  oc cluster up --version=$OPENSHIFT_VERSION
+  oc login -u system:admin
+  export REGISTRY_URL=$(oc get svc -n default docker-registry -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].port}')
+  oc login -u developer -p developer
+  oc new-project $PROJECT_NAME
+}
+
+function build() {
+  docker build -f Dockerfile.centos7 . -t $REGISTRY_URL/$PROJECT_NAME/elasticsearch:latest
+}
+
+function deploy() {
+  docker login -u `oc whoami` -p `oc whoami -t` $REGISTRY_URL
+  docker push $REGISTRY_URL/$PROJECT_NAME/elasticsearch:latest
+  oc create -f es-cluster-deployment.yml
+  oc policy add-role-to-user view -z elasticsearch
+}
+
+prepare
+
+build
+
+deploy
