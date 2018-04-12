c10c25e1951d05cd9a5d2b07a06cefc54d7c1a89
Pavol Loffay <ploffay@redhat.com>
2018-04-12 17:50:48 +0200

    Remove elasticsearch-cloud-kubernetes plugin
    elasticsearch-cloud-kubernetes is not needed,
    descovery is be done via zen plugin and k8s discovery
    
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- install.sh
+++ install.sh
@@ -3,11 +3,7 @@
 set -ex
 set -o nounset
 
-# list of plugins to be installed
-if [ -z "${ES_CLOUD_K8S_URL:-}" ] ; then
-    ES_CLOUD_K8S_URL=io.fabric8:elasticsearch-cloud-kubernetes:${ES_CLOUD_K8S_VER}
-fi
-es_plugins=($ES_CLOUD_K8S_URL)
+es_plugins=("")
 
 echo "ES plugins: ${es_plugins[@]}"
 for es_plugin in ${es_plugins[@]}
